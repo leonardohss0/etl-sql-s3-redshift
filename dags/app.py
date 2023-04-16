c493478dbbea90c66f70c6bf8e9d60dfaa9d08e6
Leonardo Sanches <leo_urania01@hotmail.com>
2023-04-16 20:44:04 -0300

    Add: Glue Job Operator

--- dags/app.py
+++ dags/app.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 from datetime import datetime, timedelta
 
 from utils.python_function import postgres_task, raw_data_to_S3
@@ -18,14 +20,31 @@ with DAG(
     default_args= default_args
 ) as dag:
 
+    # Define an PythonOperator to extract data from a Postgresql
     postgresTask = PythonOperator(
         task_id = 'postgres_task',
         python_callable= postgres_task
     )
 
+    # Define an PythonOperator to push data to a AWS S3 bucket
     rawDataToS3 = PythonOperator(
         task_id = 'raw_data_to_S3',
         python_callable= raw_data_to_S3
     )
 
-    postgresTask >> rawDataToS3
+    # Define an GlueJobOperator to run the AWS Glue job
+    glueTask = GlueJobOperator(
+        task_id='glue_task',
+        job_name='Transforming Data',  
+        aws_conn_id='s3_conn', 
+        iam_role_name='AWSGlueServiceRole-AnalyticsGlue',
+        script_location='s3://aws-glue-assets-864500937691-us-east-1/scripts/',
+        region_name= 'us-east-1'
+    )
+
+    finish = BashOperator(
+        task_id="finish",
+        bash_command="echo The job is finished",
+    )
+
+    postgresTask >> rawDataToS3 >> glueTask >> finish
